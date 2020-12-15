d7b3ff782aa673674e178dcea6b023b6bfcdc31e
dhav13 <marudhaval53@gmail.com>
2020-12-15 08:48:34 +0530

    End of Section 3

--- Startup.cs
+++ Startup.cs
@@ -38,6 +38,7 @@ namespace API
             {
                 option.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,7 +54,7 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
